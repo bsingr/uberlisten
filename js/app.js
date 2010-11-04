9b20631b4cdaa1a180b7014f4728ee3fc7af7dad
dpree <whiterabbit.init@gmail.com>
2010-11-05 00:41:56 +0100

    implemented basic CRUD

--- js/app.js
+++ js/app.js
@@ -1,12 +1,53 @@
 (function($) {
+	
+  var app = $.sammy('#container', function(app) {
+    app.use('Couch');
 
-  var app = $.sammy('#container', function() {
-    this.use('Couch');
+	Note = app.createModel("note");
+	
+	var reloadList = function(){
+	  jQuery("#list").html("<li>Loading...</li>");
+	  var ulHtml = "";
+	  Note.all(function(notes){
+		$.each(notes.rows, function(i,row){
+		  if (row.doc.type == "note") {
+			ulHtml += "<li><a href='#!/notes/"+row.id+"/delete' data-id='"+row.id+"' data-rev='"+row.doc._rev+"'>"+row.doc.text+"</a></li>";
+		  }
+		});
+		jQuery("#list").html(ulHtml);
+	  });
+	};
+
+    app.get("#!/", function(){
+	  reloadList();
+    });
+
+    app.post("#!/notes", function(){
+	  var con = this;
+	  var text = this.params["text"];
+	  Note.create({
+		"text": text
+	  },function(c){
+		con.redirect("#!/");
+	  }); 
+    });
+
+    app.get("#!/notes/:id/delete", function(){
+	  var con = this;
+	  if (window.confirm("Delete?")) {	  
+		  var doc = {
+			_id: this.params.id,
+			_rev: jQuery("[data-id='"+this.params.id+"']").attr("data-rev")
+		  };
+		  app.db.removeDoc(doc);
+      }
+      con.redirect("#!/");
+    });
 
   });
 
   $(function() {
-    app.run();
+    app.run("#!/");
   });
 
 })(jQuery);
