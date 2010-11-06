af51106a1f46c0e580ea4ebd7e6f8e7c872a7ece
dpree <whiterabbit.init@gmail.com>
2010-11-06 22:47:33 +0100

    added bulk delete; refactorings/improved UX

--- js/app.js
+++ js/app.js
@@ -5,24 +5,40 @@
 
 	Note = app.createModel("note");
 	
+	var updateForm = function(method, $note){
+	  	$("form").attr("method", method);
+		if (method == "put") {
+			$("[name=text]").val($note.attr("data-text")).select();
+			$("[name=id]").val($note.find("a").attr("data-id"));
+		}
+	};
+	
 	var reloadList = function(){
-	  jQuery("#notes").html("<li>Loading...</li>");
+	  $("#notes").html("<li>Loading...</li>");
 	  var ulHtml = "";
 	  Note.all(function(notes){
+		var count = 0;
 		$.each(notes.rows, function(i,row){
 		  if (row.doc.type == "note") {
-			ulHtml += "<li data-text='"+row.doc.text+"'>"+row.doc.text+" <a href='#!/notes/"+row.id+"/delete' data-id='"+row.id+"' data-rev='"+row.doc._rev+"'>(delete)</a></li>";
+			count++;
+			ulHtml += "<li data-text='"+row.doc.text+"' data-id='"+row.id+"' data-rev='"+row.doc._rev+"'>";
+			ulHtml += row.doc.text;
+			ulHtml += " <a href='#!/notes/"+row.id+"/delete' data-id='"+row.id+"' data-rev='"+row.doc._rev+"'>";
+			ulHtml += "&spades; delete</a></li>";
 		  }
 		});
-		jQuery("#notes").html(ulHtml);
+		$("#notes").html(ulHtml);
+		if (count > 1) { $(".notes-actions").fadeIn(300); }
+		else { $(".notes-actions").fadeOut(300); }
 	  });
 	};
 	
 	app.bind("run", function(){
-	  jQuery("#notes").delegate("li", "click", function(){
-		jQuery("[name=text]").val(jQuery(this).attr("data-text")).select();
-		jQuery("[name=id]").val(jQuery(this).find("a").attr("data-id"));
-		jQuery("form").attr("method", "put");
+	  $("#notes").delegate("li", "click", function(e){
+		updateForm("post");
+		if ($(e.target).is("li")) {
+		  updateForm("put", $(this));
+		}
 	  });
 	});
 
@@ -34,12 +50,11 @@
 	  var con = this;
 	  var text = this.params["text"];
 	  var isValid = (text != "");
-	  if (!isValid) { jQuery("[name=text]").css("background","#ffcccc"); return false; }
-	  else { jQuery("[name=text]").css("background","white").val(""); }
+	  if (!isValid) { $("[name=text]").css("background","#ffcccc"); return false; }
+	  else { $("[name=text]").css("background","white").val(""); }
 	  Note.update(this.params.id, {
 		"text": text
 	  }, function(c){
-		jQuery("form").attr("method", "post");
 		con.redirect("#!/");
 	  }); 
     });
@@ -48,8 +63,8 @@
 	  var con = this;
 	  var text = this.params["text"];
 	  var isValid = (text != "");
-	  if (!isValid) { jQuery("[name=text]").css("background","#ffcccc"); return false; }
-	  else { jQuery("[name=text]").css("background","white").val(""); }
+	  if (!isValid) { $("[name=text]").css("background","#ffcccc"); return false; }
+	  else { $("[name=text]").css("background","white").val(""); }
 	  Note.create({
 		"text": text
 	  }, function(c){
@@ -57,17 +72,32 @@
 	  }); 
     });
 
-    app.get("#!/notes/:id/delete", function(){
-	  var con = this;
-	  if (window.confirm("Delete?")) {	  
-		  var doc = {
-			_id: this.params.id,
-			_rev: jQuery("[data-id='"+this.params.id+"']").attr("data-rev")
-		  };
-		  app.db.removeDoc(doc);
-      }
+    app.get("#!/notes/:id/delete", function(con){  
+      var doc = {
+		_id: this.params.id,
+		_rev: $("[data-id='"+this.params.id+"']").attr("data-rev")
+	  };
+	  $("#notes").find("li[data-id="+this.params.id+"]").remove();
+	  app.db.removeDoc(doc, {
+		success: function(){
+		  $("body").append("<div id='msg'>Note deleted!</div>").find("#msg").fadeOut(1000, function(){$(this).remove()});
+		}
+	  });
       con.redirect("#!/");
     });
+
+    app.get("#!/notes/delete", function(con){
+	  var docs = $("#notes").find("li").map(function(){
+		return {_id: $(this).attr("data-id"), _rev: $(this).attr("data-rev")};
+	  }).get();
+	  $("#notes").empty();
+	  app.db.bulkRemove({docs: docs}, {
+	    success: function(){
+		  $("body").append("<div id='msg'>All notes deleted!</div>").find("#msg").fadeOut(1500, function(){$(this).remove()});
+		}
+	  });
+	  con.redirect("#!/");
+	});
 
   });
 
@@ -75,4 +105,4 @@
     app.run("#!/");
   });
 
-})(jQuery);
+})($);
